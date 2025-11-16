841805196277ad182739a86f4a67d2867c4502aa
mfaaizalam <mfaaiz2000bce@gmail.com>
2025-11-16 17:25:13 +0500

    login backend

--- server/models/user.js
+++ server/models/user.js
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["farmer", "expert", "admin", "buyer"],
-      default: "farmer",
+      enum: ["ranger",  "admin"],
+      default: "ranger",
     },
   },
   { timestamps: true }
