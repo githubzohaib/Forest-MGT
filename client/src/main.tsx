0d248158b5fbea6859a47325c0d61a525556f8a9
mfaaizalam <mfaaiz2000bce@gmail.com>
2025-11-17 22:40:16 +0500

    adding chat backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./index.css"
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import "./index.css"
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./index.css";
+
+// Get the root element
+const container = document.getElementById('root');
+
+// TypeScript assertion: tell TS this is not null
+if (!container) throw new Error("Root container missing in index.html");
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
